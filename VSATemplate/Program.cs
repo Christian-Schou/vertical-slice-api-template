c8e5258638c2c5d8f018a6a3526e7e6cdd661017
Poorna Soysa <poornapiumal.soysa@gmail.com>
2025-08-30 04:03:17 +0530

    Remove unwanted comments in Program.cs

--- VSATemplate/Program.cs
+++ VSATemplate/Program.cs
@@ -1,8 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly;
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
