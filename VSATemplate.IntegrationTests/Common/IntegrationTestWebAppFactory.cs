92585b00c1356d478db2455a218185c13d9a9823
Christian Schou <christian.schou@hotmail.com>
2026-01-12 08:29:11 +0100

    👔 Add base integration test setup with PostgreSQL container
    Introduces BaseIntegrationTest and IntegrationTestWebAppFactory to support integration testing. The setup uses Testcontainers to spin up a PostgreSQL container and Respawn to reset the database state between tests.

---
+++ VSATemplate.IntegrationTests/Common/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Testcontainers.PostgreSql;
+
+namespace VSATemplate.IntegrationTests.Common;
+
+public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:15-alpine")
+        .Build();
+
+    public Task InitializeAsync()
+    {
+        return _dbContainer.StartAsync();
+    }
+
+    public new Task DisposeAsync()
+    {
+        return _dbContainer.StopAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseSetting("ConnectionStrings:Database", _dbContainer.GetConnectionString());
+    }
+}
